45763efb027715ab63f7375d731ba4c09c517c8e
Bryan Yehuda <bryanyehuda@gmail.com>
2020-10-14 20:29:17 +0700

    Final Update to the Repository

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/bryan/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/bryan/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/bryan/catkin_ws/devel;/opt/ros/melodic" != "" else []
